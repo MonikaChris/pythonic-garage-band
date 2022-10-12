08817b4a12efd2d3ebffba1e0f274ae90e266451
Monika <monidavies@gmail.com>
2022-10-12 16:38:03 -0700

    Adds to_list method

--- band.py
+++ band.py
@@ -1,13 +1,21 @@
 class Band:
+    instances = []
+
     def __init__(self, name, members):
         self.name = name
         self.members = members
+        Band.instances.append(self)
 
     def play_solos(self):
         solos = []
         for member in self.members:
             solos.append(member.play_solo())
         return solos
+
+    @classmethod
+    def to_list(cls):
+        return Band.instances
+
 
 class Musician:
     pass
