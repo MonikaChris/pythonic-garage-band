70865257a3106c795ed6df06461cbf9ce8c13e74
Monika <monidavies@gmail.com>
2022-10-12 15:50:20 -0700

    Adds guitarist repr method

--- band.py
+++ band.py
@@ -13,6 +13,9 @@ class Guitarist(Musician):
     def __str__(self):
         return f'My name is {self.name} and I play guitar'
 
+    def __repr__(self):
+        return f'Guitarist instance. Name = {self.name}'
+
 
 class Bassist(Musician):
     def __init__(self, name):
