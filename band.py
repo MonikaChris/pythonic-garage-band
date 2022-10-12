c072e6726d2b3f6ccd5d4823f7769c44e8fb0b4d
Monika <monidavies@gmail.com>
2022-10-12 15:52:00 -0700

    Adds drummer str method

--- band.py
+++ band.py
@@ -25,3 +25,6 @@ class Bassist(Musician):
 class Drummer(Musician):
     def __init__(self, name):
         self.name = name
+
+    def __str__(self):
+        return f'My name is {self.name} and I play drums'
